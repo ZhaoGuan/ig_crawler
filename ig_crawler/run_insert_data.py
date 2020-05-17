d3744d5275093b1c1c4b3ad0f1619825efb3d1d0
ZhaoGuan <guanzhaogz@gmail.com>
2020-05-17 12:41:57 +0800

    save

--- ig_crawler/run_insert_data.py
+++ ig_crawler/run_insert_data.py
@@ -3,6 +3,8 @@
 # __author__ = 'Gz'
 from tools.data_insert import insert_all
 import os
+import threadpool
+import time
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 data_path = PATH + "/data"
@@ -11,3 +13,5 @@ for file in files:
     file_path = os.path.abspath(data_path + "/" + file)
     print(file_path)
     insert_all(file_path)
+    print("等待10s")
+    time.sleep(10)
